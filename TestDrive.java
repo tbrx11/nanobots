3fd014253ef12b8d8e5bd6540ca3ee0f4052c76d
tbrx11 <92124476+tbrx11@users.noreply.github.com>
2022-11-20 20:21:44 -0500

    Update TestDrive.java
    Signed-off-by: tbrx11 <92124476+tbrx11@users.noreply.github.com>

--- TestDrive.java
+++ TestDrive.java
@@ -1,15 +1,14 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @TeleOp(name="TestDrive", group="Linear Opmode")
-
 public class TestDrive extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
@@ -18,8 +17,6 @@ public class TestDrive extends LinearOpMode {
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
     private DcMotor rightBack = null;
-    private int precision;
-    private Boolean precision_check;
 
     @Override
     public void runOpMode() {
@@ -31,13 +28,10 @@ public class TestDrive extends LinearOpMode {
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
 
         //motor direction
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
         leftBack.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
-        rightBack.setDirection(DcMotor.Direction.REVERSE);
-
-        precision = 0;
-        precision_check = false;
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -81,56 +75,16 @@ public class TestDrive extends LinearOpMode {
                 rightBackPower  /= max;
             }
 
-            //prec = 1 is 1/2 speed / prec= 2 is 1/3 speed
-            if (gamepad1.left_stick_button) {
-                if (!precision_check) {
-                    if (precision == 2) {
-                        precision = 0;
-                    } else {
-                        precision = 2;
-                    }
-                }
-                precision_check = true;
-            } else if (gamepad1.right_stick_button) {
-                if (!precision_check) {
-                    if (precision == 3) {
-                        precision = 0;
-                    } else {
-                        precision = 3;
-                    }
-                }
-                precision_check = true;
-            } else {
-                precision_check = false;
-            }
-
-            //Send calculated power to wheels
-            if(precision == 2) {
-                leftFront.setPower(leftFrontPower/ 2);
-                rightFront.setPower(rightFrontPower/ 2);
-                leftBack.setPower(leftBackPower/ 2);
-                rightBack.setPower(rightBackPower/ 2);
-            }
-            else if(precision == 3){
-                leftFront.setPower(leftFrontPower/ 3);
-                rightFront.setPower(rightFrontPower/ 3);
-                leftBack.setPower(leftBackPower/ 3);
-                rightBack.setPower(rightBackPower/ 3);
-            }
-            else if(precision == 0){
-                leftFront.setPower(leftFrontPower);
-                rightFront.setPower(rightFrontPower);
-                leftBack.setPower(leftBackPower);
-                rightBack.setPower(rightBackPower);
-            }
-
-
+            // Test Send calculated power to wheels (get rid of /2)
+            leftFront.setPower(leftFrontPower/2);
+            rightFront.setPower(rightFrontPower/2);
+            leftBack.setPower(leftBackPower/2);
+            rightBack.setPower(rightBackPower/2);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front Left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back Left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Precision mode:", "1/" + precision);
             telemetry.update();
         }
     }}
